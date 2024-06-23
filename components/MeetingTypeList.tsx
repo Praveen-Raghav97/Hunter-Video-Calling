7775034fbfd54be0b6ecdf9aa9f9208d9b4ee09e
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-06-23 17:37:24 +0530

    chnge

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -172,7 +172,7 @@ const MeetingTypeList = () => {
 
         image="/icons/checked.svg"
         buttonIcon ="/icons/copy.svg"
-        buttonText = "Copy Meeting Link"
+      
 
         />
       )
