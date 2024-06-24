2acbe90c9a8e21d0b0f3a6e3ea92c524df23c20d
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-06-24 09:23:01 +0530

    change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
     bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href='/' className = 'flex item-center gap-1'>
       <Image
-      src="/icons/menu.png" 
+      src="/icons/logo.svg" 
       width={32}
       height={32}
-      alt='Yoom logo'
+      alt='Hunter logo'
       className='max-sm:size-10'
       />
       <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Hunter</p>
